79526aca907ed973be5826ad6aaa78a434f31708
Diego Sosa <dhsosa98@gmail.com>
2021-12-30 18:49:33 -0300

    Created new op controller for search & filter op

--- api/controllers/operations.controller.js
+++ api/controllers/operations.controller.js
@@ -1,4 +1,6 @@
 const { operationRepository } = require('../../dal/repositories'); 
+const { Op } = require("sequelize");
+const getTotal = require('../middlewares/getTotal');
 
 const getAll = async (req, res) => {
 	const operations = await operationRepository.getOperations(); 
@@ -45,17 +47,31 @@ const updateOperation = async (req, res) => {
 
 const getByUserID = async (req, res) => {
 	const { id } = req.session.user; 
-	let {page, size} = req.query
+	let {page, size, sort, search} = req.query
+	console.log(sort) 
+	let sortOpt = []
+	sort?.split('#').forEach((element)=>{
+		sortOpt = [...sortOpt, [element?.split("%")[0], element?.split("%")[1]]]
+	})
 	let limit = parseInt(size) || 10
 	const offset = parseInt(page)*size || 0
 	let toWhere = { UserId: id };
-	let options = {where: toWhere}
-	if (offset){options = {...options, offset: offset}}
-	if (limit){options = {...options, limit: limit}}
+	let options = {};
+	if (search){
+		toWhere = {...toWhere, [Op.or]: [ {concept: { [Op.like]: '%'+search+'%'} }, {category: { [Op.like]: '%'+search+'%'} } ]}
+	}
+	else{
+		options = {...options, offset: offset}
+	}
+	options = {...options, where: toWhere}
+	if (sort){options = {...options, order: sortOpt}}
+	options = {...options, limit: limit}
 	console.log(options)
-	const operations = await operationRepository.getAllByFieldAndPagination(options); 
+	const total = await getTotal(id)
+	const operations = await operationRepository.getAllByFieldAndPagination(options) || []; 
+	operations["total"] = total || 0
 	return res.send({
-		data: operations
+		data: {operations: operations}
 	});
 };
 
