646567c1c47e1fa1430b2d98b9b1c9543c4c2e4b
Diego Sosa <dhsosa98@gmail.com>
2021-12-30 18:46:08 -0300

    added new env config for modes prod and dev

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 require('dotenv').config(); 
 module.exports = {
 	'development': {
-		'username': process.env.DB_USER,
-		'password': process.env.DB_PASSWORD,
-		'database': process.env.DB_DATABASE,
-		'host': process.env.DB_HOST,
-		'dialect': process.env.DB_DIALECT
+		'username': process.env.DB_USER_DEV,
+		'password': process.env.DB_PASSWORD_DEV,
+		'database': process.env.DB_DATABASE_DEV,
+		'host': process.env.DB_HOST_DEV,
+		'dialect': process.env.DB_DIALECT_DEV
 	},
 	'test': {
 		'username': process.env.DB_USER,
