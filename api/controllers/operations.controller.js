e0131a13baa7fb93d73c854bca32a9cd18c87a80
Diego Sosa <dhsosa98@gmail.com>
2022-01-02 00:38:22 -0300

    added query for search by dates

--- api/controllers/operations.controller.js
+++ api/controllers/operations.controller.js
@@ -1,6 +1,7 @@
 const { operationRepository } = require('../../dal/repositories'); 
 const { Op } = require("sequelize");
 const getTotal = require('../middlewares/getTotal');
+const Sequelize = require('sequelize');
 
 const getAll = async (req, res) => {
 	const operations = await operationRepository.getOperations(); 
@@ -47,8 +48,7 @@ const updateOperation = async (req, res) => {
 
 const getByUserID = async (req, res) => {
 	const { id } = req.session.user; 
-	let {page, size, sort, search} = req.query
-	console.log(sort) 
+	let {page, size, sort, search, from, to} = req.query
 	let sortOpt = []
 	sort?.split('#').forEach((element)=>{
 		sortOpt = [...sortOpt, [element?.split("%")[0], element?.split("%")[1]]]
@@ -58,10 +58,30 @@ const getByUserID = async (req, res) => {
 	let toWhere = { UserId: id };
 	let options = {};
 	if (search){
-		toWhere = {...toWhere, [Op.or]: [ {concept: { [Op.like]: '%'+search+'%'} }, {category: { [Op.like]: '%'+search+'%'} } ]}
+		if (!!search.trim()){
+		toWhere = {...toWhere, [Op.or]: [ {concept: { [Op.like]: '%'+search.trim()+'%'} }, {category: { [Op.like]: '%'+search.trim()+'%'} } ] }
+		}
+		else{
+			search = search.trim()+' '
+			toWhere = {...toWhere, 
+				[Op.or]: [ 
+					{ [Op.or]: 
+						[ { concept: { [Op.eq]: search.trim() } } , { concept: {  [Op.like]: '%'+search+'%' } } ] } ,
+					{ [Op.or]: 
+						[ { category: { [Op.eq]: search.trim() } } , { category: {  [Op.like]: '%'+search+'%' } } ] 
+					} ] }
+		}
 	}
 	else{
 		options = {...options, offset: offset}
+	}
+	if (from){
+		if (!to){
+		toWhere = {...toWhere, [Op.and]: [{date: {[Op.between]: [from, Sequelize.fn('NOW')]}}]}
+		}
+		else{
+			toWhere = {...toWhere, [Op.and]: [{date: {[Op.between]: [from, to]}}]}
+		}
 	}
 	options = {...options, where: toWhere}
 	if (sort){options = {...options, order: sortOpt}}
