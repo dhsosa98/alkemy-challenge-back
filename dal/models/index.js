4dca276236b0892f30a23792f3c06b097e227027
Diego Sosa <dhsosa98@gmail.com>
2022-01-03 00:24:01 -0300

    changed env variables

--- dal/models/index.js
+++ dal/models/index.js
@@ -5,13 +5,15 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV;
 const config = require(__dirname + '/../../config/config')[env];
+console.log(config)
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
 	sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-	sequelize = new Sequelize(config.database, config.username, config.password, config);
+	console.log('Hola')
+	sequelize = new Sequelize(config.database, config.username, config.password, {host: config.host, dialect: config.dialect, port: '3306'});
 }
 
 fs
