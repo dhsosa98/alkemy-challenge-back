8de443e13db99e8afe0d8b158fa928420c55a7ac
Diego <dhsosa98@gmail.com>
2024-02-09 21:32:28 -0300

    add typescript

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import dotenv from 'dotenv';
-import db from './dal/models/index.js';
-import App from './app.js';
+import App from './app';
 
 dotenv.config();
 
@@ -8,6 +7,4 @@ const port = parseInt(process.env.PORT);
 
 App.listen(port, function(){
 	console.log(`Application running on ${port}`);
-	console.log(Object.keys(db));
-	db.sequelize.sync();
 });
